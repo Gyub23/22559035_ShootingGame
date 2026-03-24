5c6d481f301b808ba56f338561462636bfc2adff
Gyub23 <gongsa23gg@gmail.com>
2026-03-25 00:28:35 +0900

    3주차수업과제
    3주차수업과제

---
+++ Assets/Scripts/Test_Scripts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Scripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Developer Tree Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
